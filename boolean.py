785a1524daca6ca88d5c94a0b1772f5e89e73da4
Takayuki Nakatsuji <129576342+takayuki91@users.noreply.github.com>
2023-08-07 12:15:09 +0900

    [Add]relational_operator.py

---
+++ boolean.py
@@ -0,0 +1,11 @@
+# boolean型:True False
+
+print(True)  #最初は大文字
+print(False)
+
+balance = 1000
+withdrawal = 500
+print(balance < withdrawal)
+
+print(True + True)
+print(True + False)
---
+++ relational_operator.py
@@ -0,0 +1,11 @@
+# 比較演算子（relational operator） > < >= <= == !=
+print(5 > 3)
+print(2.0 < 2.0)
+print(5 <= 5)
+print(5 >= 5)
+print(5 == 3)
+print(5 != 3)
+
+print(1 == 1.0)
+print(1 == "1")
+print("1" == "1")
