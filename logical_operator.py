9ae1ca3ed76429a6455511767f28872ff10ba172
Takayuki Nakatsuji <129576342+takayuki91@users.noreply.github.com>
2023-08-07 12:38:34 +0900

    [Add]logical_operator.py

---
+++ logical_operator.py
@@ -0,0 +1,30 @@
+# 論理演算子（logical operator）
+# and or not
+
+a = 1
+b = 1
+c = 10
+d = 100
+print(a == 1 and c > d)
+print(a == 1 or c > d)
+print(not a == b)
+
+# 演習
+a = 10
+b = 110
+c = 33
+d = 178
+print(c >= a and d >= b)
+
+hakasego = True
+keikenn = 6
+print(hakasego == True and keikenn >= 5)
+
+# 答え
+age = 11
+height = 110
+print(age >= 10 and height >= 110)
+
+master = True
+job_experience = 3
+print(master or job_experience > 5)
