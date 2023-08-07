9c1e3f08c3d385de6842a1ba68ebb40899fcdd3e
Takayuki Nakatsuji <129576342+takayuki91@users.noreply.github.com>
2023-08-07 13:06:28 +0900

    [Add]if.py

---
+++ if.py
@@ -0,0 +1,40 @@
+# if文
+
+age = 120
+age_alcohol = 21
+if age >= age_alcohol:
+    print("You can drink beer!")
+else:
+    print("You are too young to drink beer")
+
+age_drive = 18
+if age >= age_alcohol:
+    print("You can drink beer!")
+elif age < age_drive:
+    print("You cannot even drive!")
+else:
+    print("You are not allowed to drink beer but you can drive only if you have a driver's license")
+
+if not 0 < age < 120:
+    print("The value is invalid!!")
+
+
+# 演習
+balance = 100000
+withdrawal = 1000000
+# if balance > withdrawal:
+#     balance -= withdrawal
+#     print(f"Your new balance is {balance}")
+# else:
+#     print("You cannot withdraw")
+
+# 演習2
+withdrawal_limit = 1000000
+
+if withdrawal > withdrawal_limit:
+    print("The withdrawal limit is {}".format(withdrawal_limit))
+elif balance > withdrawal:
+    balance -= withdrawal
+    print("Your new balance is {}".format(balance))
+else:
+    print("You cannot withdraw")
