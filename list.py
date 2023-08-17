25874f0d903363b4f78f40d10a69ff9d86491475
Takayuki Nakatsuji <129576342+takayuki91@users.noreply.github.com>
2023-08-17 22:41:40 +0900

    [Add]list.py

---
+++ list.py
@@ -0,0 +1,9 @@
+
+fruits = ['apple', 'peach', 'melon', 'grapes']
+hetro_list = ['string', 1, 3.4, True, fruits]
+
+print(hetro_list)
+print(fruits[0])
+print(fruits[-3])
+print(hetro_list[-1][-1])
+print("hello world"[3])
