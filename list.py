e7ed3a4fd3ae05e37bce90eb02c623a9a578ac95
Takayuki Nakatsuji <129576342+takayuki91@users.noreply.github.com>
2023-08-18 16:26:31 +0900

    [Add]list.py-method

--- list.py
+++ list.py
@@ -1,9 +1,29 @@
 
 fruits = ['apple', 'peach', 'melon', 'grapes']
 hetro_list = ['string', 1, 3.4, True, fruits]
+#
+# print(hetro_list)
+# print(fruits[0])
+# print(fruits[-3])
+# print(hetro_list[-1][-1])
+# print("hello world"[3])
 
-print(hetro_list)
-print(fruits[0])
-print(fruits[-3])
-print(hetro_list[-1][-1])
-print("hello world"[3])
+# Listのメソッド
+print(fruits)
+fruits.append('banana')
+print(fruits)
+
+fruits.insert(3, 'lemon')
+print(fruits)
+
+fruits.remove('peach')
+print(fruits)
+
+fruits.sort()
+print(fruits)
+
+fruits.sort(reverse=True)
+print(fruits)
+
+print(len(fruits))
+print(len("hello world"))
