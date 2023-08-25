3f1855440b0a54c566d99ecc4a38555b9d6ceff4
Takayuki Nakatsuji <129576342+takayuki91@users.noreply.github.com>
2023-08-25 22:37:32 +0900

    [Add]range.py

---
+++ range.py
@@ -0,0 +1,17 @@
+# range(start,stop,step)
+
+# for _ in range(10):
+#     print("hello")
+
+# FizzBuzz
+for i in range(1, 51):
+    if i % 15 == 0:
+        print("FizzBuzz")
+    elif i % 3 == 0:
+        print("Fizz")
+    elif i % 5 == 0:
+        print("Buzz")
+    else:
+        print(i)
+
+
